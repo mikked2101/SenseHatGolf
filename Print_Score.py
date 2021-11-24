c8a413d4ebcfa489328a1ec2d0f906e4eaa65c52
chrlindh <91655250+chrlindh@users.noreply.github.com>
2021-11-24 15:21:06 +0100

    Update Print_Score.py

--- Print_Score.py
+++ Print_Score.py
@@ -1,4 +1,6 @@
 def print_score(score):                     #Funksjon som printer ut score
     with open("yourscore.txt", "w") as fil: #Åpner fil med navn yourscore som tillater å skrive i fil
         fil.write("Score: " + str(score))   #Score blir lagt til i fil
+score = mynt + seier
+print_score(score)
 
