4b6f3d7f610513530da28199ded5c787b6637078
Mikkel Blomnes <78788768+mikked2101@users.noreply.github.com>
2021-11-28 14:55:05 +0100

    Update spillfunksjoner.py

--- spillfunksjoner.py
+++ spillfunksjoner.py
@@ -52,58 +52,5 @@ def nextlevel(level):
         
                            
         
-        
-        
- def LOAD_LEVEL():
-                           import numpy as np
-from sense_hat import SenseHat
 
-sense=SenseHat()
-
-
-
-#Level 2
-
-blank2 =         ([0, 0, 0, 0, 0, 0, 0, 0],
-                  [2, 2, 2, 2, 2, 2, 2, 0],
-                  [0, 2, 0, 0, 0, 0, 0, 0],
-                  [0, 2, 0, 2, 0, 2, 2 ,2],
-                  [0, 2, 0, 0, 0, 0, 2, 0],
-                  [0, 2, 0, 2, 0, 2, 0, 2],
-                  [0, 2, 0, 2, 0, 0, 0, 0],
-                  [0, 0, 0, 2, 0, 2, 0, 0]
-                  )
-
-level2=np.array(blank2)
-
-ballpos = bx, by = 1, 1 # Starting positions
-
-start=sx, sy=0, 0
-finish=fx, fy=7, 7
-
-
-
-level2[start] = 1
-level2[finish] = -1
-
-
-W=(192 ,192 ,192)#Wall
-H=(255, 0, 0)#Hole
-F=(255, 255, 0)#Finish
-G=(0, 0, 0)#Ground
-
-
-
-for i in range(8):
-    for j in range(8):
-        if level2[i ,j]==2:
-            sense.set_pixel(i ,j ,W)
-        elif level2[i ,j]==3:
-            sense.set_pixel(i ,j ,H)
-        elif level2[i ,j]==-1:
-            sense.set_pixel(i ,j ,F)
-        else:
-            sense.set_pixel(i ,j ,G)
-            
-        
 
