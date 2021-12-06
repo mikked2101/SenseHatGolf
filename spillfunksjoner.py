8446d411f6bed6e95bcde76d959e58f218e1bfb6
fredrki <91655281+fredrki@users.noreply.github.com>
2021-12-06 11:28:14 +0100

    Update spillfunksjoner.py

--- spillfunksjoner.py
+++ spillfunksjoner.py
@@ -62,6 +62,41 @@ def nextlevel(level):
                 [0, 2, 2, 2, 2, 3, 0, 3],
                 [1, 2, -1, 0, 0, 0, 0, 0]) 
 
+    if level == 5:
+    
+        return  ([3,0,0,0,0,0,0,3],
+                 [0,0,2,0,3,2,0,3],
+                 [0,3,2,0,3,2,0,0],
+                 [0,0,2,0,0,2,3,0],
+                 [3,0,2,3,0,2,0,0],
+                 [-1,0,2,0,0,2,3,4],
+                 [3,3,2,0,3,2,3,3],
+                 [1,0,0,0,0,0,0,3]
+                 )
+    if level == 6:
+    
+        return  ([1,2,3,3,3,3,3,3],
+                 [0,2,-1,0,0,0,0,0],
+                 [0,2,2,3,2,2,3,0],
+                 [0,2,0,0,0,0,0,0],
+                 [0,2,0,3,2,2,3,2],
+                 [0,2,0,0,0,0,0,4],
+                 [0,3,2,2,3,3,0,3],
+                 [0,0,0,0,0,0,0,0]
+                 )
+    if level == 7:
+    
+        return  ([1,2,-1,0,0,0,0,3],
+                 [0,2,2,2,2,2,0,0],
+                 [0,2,3,3,3,3,3,0],
+                 [0,2,3,3,2,0,0,0],
+                 [0,2,0,4,3,0,3,3],
+                 [0,2,0,0,2,0,0,0],
+                 [0,2,0,3,3,3,3,0],
+                 [0,0,0,0,0,0,0,0]
+                 )
+
+
 """def get_positions(level):
     holepos = []
         
@@ -91,6 +126,16 @@ def print_score(score):                                                         
     now1 = now.strftime("%d-%m-%Y %H:%M:%S")                                                #finere format av dato og tid
     with open("yourscore.txt", "a") as fil:                                                 #AApner fil med navn yourscore som tillater aa skrive i fil
         fil.write("\n" + "Current date and time: " + str(now1) + " Score: " + str(score))   #Score blir lagt til i fil med dato
+        
+def Victory(score):
+    sense.clear
+    sense.show_message("You Win!",scroll_speed=0.02,text_colour=[128,128,128])
+    sense.show_message("Your score: "+ str(score),scroll_speed=0.02,text_colour=[128,128,128])
+    print_score(score)
+    for i in range (0,24):
+        choice="Victoryscreen"+str(i)+".png"
+        sense.load_image(choice)
+        time.sleep(0.05)
 
 def ori():
     orientation = sense.get_orientation()
